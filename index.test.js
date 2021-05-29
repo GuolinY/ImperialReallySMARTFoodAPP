93edd0b727f993688e5919f7d719ac6be7ea58cf
Xiaoning He <xhe123@hotmail.co.uk>
2021-05-29 12:39:04 +0100

    Add tests

---
+++ index.test.js
@@ -0,0 +1,15 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { render, screen } from "@testing-library/react";
+import Home from "./pages/index";
+
+describe("App", () => {
+  it("renders without crashing", () => {
+    render(<Home />);
+    expect(
+      screen.getByRole("heading", { name: "Welcome!" })
+    ).toBeInTheDocument();
+  });
+});
