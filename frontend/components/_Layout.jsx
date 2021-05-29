5d382019cc317f8f4b130cbad7a773deef914476
Xiaoning He <xhe123@hotmail.co.uk>
2021-05-29 10:49:22 +0100

    NextJS Setup

---
+++ frontend/components/_Layout.jsx
@@ -0,0 +1,30 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Link from "next/link";
+import { IconButton } from "@material-ui/core";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+
+export default function Layout({ title = "Xiaoning's Website", children }) {
+  const redirect = (url) => {
+    console.log(url);
+    window.location = url;
+  };
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>{title}</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta charSet="utf-8" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>{children}</main>
+
+      <footer className={styles.footer}>
+        Really Smart Food App || Group 47
+      </footer>
+    </div>
+  );
+}
---
+++ frontend/pages/index.jsx
@@ -0,0 +1,16 @@
+import styles from "../styles/Home.module.css";
+import Layout from "../components/_Layout";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <Layout>
+      <h2 className={styles.title}>Welcome!</h2>
+      <div>
+        <Link href="/example">
+          <a>What would you like to eat today?!</a>
+        </Link>
+      </div>
+    </Layout>
+  );
+}
